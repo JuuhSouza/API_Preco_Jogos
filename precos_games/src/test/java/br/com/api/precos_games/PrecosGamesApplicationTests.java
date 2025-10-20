320acc35da7525ff241ab25f25aaaf4eb729e351
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-10-20 11:50:19 -0300

    Add files via upload

---
+++ precos_games/src/test/java/br/com/api/precos_games/PrecosGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.precos_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecosGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
