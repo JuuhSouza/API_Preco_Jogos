b5ed813be41b5c5d3cf0d0238ce7a03ad6b42cc1
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-10-24 08:14:06 -0300

    Delete api_precoJogo directory

--- api_precoJogo/src/main/java/br/com/teste/api_precoJogo/conversor/ConversorDolar.java
+++
@@ -1,8 +0,0 @@
-package br.com.teste.api_precoJogo.conversor;
-
-import br.com.teste.api_precoJogo.ApiPrecoJogoApplication;
-import com.google.gson.Gson;
-
-public class ConversorDolar {
-
-}
--- api_precoJogo/src/main/java/br/com/teste/api_precoJogo/service/IcoverteDados.java
+++
@@ -1,5 +0,0 @@
-package br.com.teste.api_precoJogo.service;
-
-public interface IcoverteDados {
-    <T> T obterDados(String json, Class<T> tClass);
-}
