f77e4714643700396c172380903344ca4335b5a3
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-10-27 08:11:28 -0300

    Add files via upload

---
+++ API_Preco_Jogos-main/README.md
@@ -0,0 +1,36 @@
+# 🎮 CheapShark API - Consulta de Preços de Jogos
+
+Projeto desenvolvido em Java que consome a API do CheapShark, permitindo buscar informações e preços de jogos digitais em diversas lojas online.  
+Ideal para quem quer comparar valores de games e verificar promoções em tempo real.
+
+## 🚀 Funcionalidades
+
+- 🔍 Buscar informações de um jogo pelo nome  
+- 💲 Mostrar o preço em dólar e converter para real (R$)  
+- 🏷️ Exibir descontos e lojas disponíveis  
+- 🧾 Retornar detalhes como título, preço em dolar, preço em reais, foto do game, ID do jogo e ele faz uma organização do menor ao maior preço. 
+
+---
+
+## 🛠️ Tecnologias Utilizadas
+
+- **Java 17+  
+- HTTPClient (para consumir a API)  
+- JSON / Jackson (para tratar os dados retornados)  
+- CheapShark API  
+- IntelliJ IDEA como ambiente de desenvolvimento  
+
+---
+
+## 🔗 API Utilizada
+
+- CheapShark API
+  Documentação oficial: [https://apidocs.cheapshark.com](https://apidocs.cheapshark.com)
+
+Link da apresentação no canva : https://www.canva.com/design/DAG2pPk5710/Q41_183UCTCGcLHCM2RBrg/edit?utm_content=DAG2pPk5710&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+
+⚙️ Como Executar o Projeto
+
+1. **Clone este repositório:**
+   bash
+   git clone https://github.com/seuusuario/nome-do-repositorio.git
---
+++ API_Preco_Jogos-main/api_precoJogo/src/main/java/br/com/teste/api_precoJogo/main/Main.java
@@ -0,0 +1,69 @@
+package br.com.teste.api_precoJogo.main;
+import br.com.teste.api_precoJogo.model.GameInformations;
+import br.com.teste.api_precoJogo.service.APIConsumption;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.*;
+
+public class Main {
+
+    private Scanner scanner = new Scanner(System.in);
+    private final String ADDRESS = "https://www.cheapshark.com/api/1.0/games?title=";
+    private final double realQuote = 5.75;
+
+    public void showMenu() {
+        while (true) {
+        try {
+            APIConsumption APIConsumption = new APIConsumption();
+            System.out.println("      -------------------------------------------------------");
+            System.out.print("      Digite um nome de jogo para procurar os melhores preços:");
+            System.out.println("\n      Digite sair para encerra a pesquisa");
+            System.out.println("      -------------------------------------------------------");
+            String name = scanner.nextLine();
+
+            // Evita erro com espaços
+            String json = APIConsumption.getData(ADDRESS + name.replace(" ", "+"));
+
+            ObjectMapper mapper = new ObjectMapper();
+            List<GameInformations> games = mapper.readValue(json, new TypeReference<List<GameInformations>>() {});
+
+            //Caso o usurio dizegite errado
+            if (games.isEmpty()){
+                System.out.println("****** Nenhum jogo encontrado, tente novamente ******");
+                return;
+            }
+
+            //opção de saida
+            if (name.equalsIgnoreCase("sair")){
+                System.out.println("Programa encerrado com sucesso!");
+                break;
+            }
+
+            //Ordena pelo menor preço
+            games.sort(Comparator.comparingDouble(j -> Double.parseDouble(j.valor())));
+
+            System.out.println("-----------------------------");
+            System.out.println("Melhores preços do jogo: " + name);
+            System.out.println("-----------------------------");
+
+            for (GameInformations game : games) {
+                //CONVERSOR DOLAR PARA O REAL
+                double dollarPrice = Double.parseDouble(game.valor()); // mudar a variavel string da api para double
+                double realPrice = dollarPrice * realQuote;
+                String formattedPrice = String.format("R$%.2f", realPrice); // formatar o preço para virgula e só aparecer as 2 primeiras casas depois da virgula
+
+                System.out.println("Título: " + game.titulo());
+                System.out.println("Preço em dolar: $" + game.valor());
+                System.out.println("Preço em reais: " + formattedPrice);
+                System.out.println("Foto do jogo: " + game.foto());
+                System.out.println("Id do jogo: " + game.idJogo());
+                System.out.println("Id da Steam: " + game.idSteam());
+                System.out.println("-----------------------------");
+            }
+        }catch (Exception e) {
+            System.out.println("Erro ao buscar ou processar dados da API:" + e.getMessage());
+        }
+    }
+    }
+}
